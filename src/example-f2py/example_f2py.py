2d3df75784a46dfea6a3f1892ac22606626b9f15
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2023-04-17 23:20:24 +0200

    add and clean examples

--- src/example-f2py/example_f2py.py
+++ src/example-f2py/example_f2py.py
@@ -4,21 +4,13 @@ f2py makes it possible to call Fortran 77/90/95 external subroutines and
 Fortran 90/95 module subroutines. Derived data types are not supported.
 
 @note:
-f2py tries to be clever/pythonic and converts subroutines into functions, and
+- f2py tries to be clever/pythonic and converts subroutines into functions, and
 tries to find optional arguments. As a result, the signature of the function
 in python is not necessarily the original one.
+- abstract interfaces are not accepted (fortran 2003).
 
-Build process (see ./fortran/build.bat):
-
-- Quick way: 1-step procedure
-f2py -c filename.f90 -m modulename
-
-- Smart way: 2-step procedure
-f2py filename.f90 -m modulename -h filename.pyf  (check signature)
-f2py -c filename.pyf filename.f
-
-To keep in mind:
-- The dll file MUST be placed next to the pyd file.
+Build process:
+ - See ./fortran/build.bat
 
 Sources:
 - https://numpy.org/doc/stable/f2py/index.html
@@ -28,29 +20,67 @@ Sources:
 from fortranmodule import mathtools
 import numpy as np
 
-# %% vectorsum (single precision)
-# Note how f2py changed the signature of the original subroutine
+# %% intsum
+
+print('\n'*3, mathtools.intsum.__doc__)
+a = 2
+b = 3
+result = mathtools.intsum(a, b)
+print('result type: ', type(result))
+print('result: ', result)
+
+# %% real4sum
+
+print('\n'*3, mathtools.real4sum.__doc__)
+
+result = mathtools.real4sum(a, b)
+print('result type: ', type(result))
+print('result: ', result)
+
+# %% real8sum
+
+print('\n'*3, mathtools.real8sum.__doc__)
+
+result = mathtools.real8sum(a, b)
+print('result type: ', type(result))
+print('result: ', result)
+
+
+# %% vectorsum
 
 print('\n'*3, mathtools.vectorsum.__doc__)
 
-a = np.arange(1, 6, dtype=np.float64)
+a = np.arange(1, 6, dtype=np.float32)
 b = a + 1
 
 result = mathtools.vectorsum(a, b)
-
 print('result type: ', result.dtype)
-print('result: ', result)
+print('result: \n', result)
 
-# %% vectorproduct (double precision)
+# %% matrixsum
 
-print('\n'*3, mathtools.vectorproduct.__doc__)
+print('\n'*3, mathtools.matrixsum.__doc__)
 
-result = mathtools.vectorproduct(a, b)
+a = np.ones((2, 3), dtype=np.float64)
+b = 1/2*np.ones_like(a)
 
+result = mathtools.matrixsum(a, b)
 print('result type: ', result.dtype)
-print('result: ', result)
+print('result: \n', result)
 
-# %% saxpy (result returned via argument update)
+# %% matrixtimesvector
+
+print('\n'*3, mathtools.matrixtimesvector.__doc__)
+
+a = np.ones((2, 3), dtype=np.float64)
+b = 1/2*np.ones(3, dtype=np.float64)
+
+result = mathtools.matrixtimesvector(a, b)
+
+print('result: \n', result)
+
+# %% saxpy
+# result returned via argument update
 
 print('\n'*3, mathtools.saxpy.__doc__)
 
@@ -59,38 +89,18 @@ x = np.ones(5, dtype=np.float64)
 y = 2*np.ones(5, dtype=np.float64)
 
 _ = mathtools.saxpy(a, x, y)
-
 print('result (y): ', y)
 
-# %% matrixpartialsum
-# The input may have order='C' or order='F', but the output has order='F'.
+# %% averagefnc
+# example with callback function
 
-print('\n'*3, mathtools.matrixpartialsum.__doc__)
-
-a = np.ones((2, 3), dtype=np.float64, order='C')
-
-result = mathtools.matrixpartialsum(a)
-
-print('result type: ', result.dtype)
-print('result has fortran order: ', np.isfortran(result))
-print('result: \n', result)
-
-# %% intproduct
-# The result is recast to float!!!
-
-print('\n', mathtools.intproduct.__doc__)
-
-a = 2
-b = 3
-
-result = mathtools.intproduct(a, b)
-print('result type: ', type(result))
-print('result: ', result)
+print('\n'*3, mathtools.averagefnc.__doc__)
 
 
-# %% intproduct2
-# This works well, despite the warning by f2py
+def fx(x: float): return x**2
 
-result = mathtools.intproduct2(a, b)
-print('result type: ', type(result))
+
+a = 1
+b = 2
+result = mathtools.averagefnc(fx, a, b)
 print('result: ', result)
--- src/example-f2py/fortran/mathtools.f90
+++ src/example-f2py/fortran/mathtools.f90
@@ -3,18 +3,32 @@ module mathtools
    use, intrinsic :: iso_fortran_env, only: real32, real64
    implicit none
    private
-   public :: vectorproduct, vectorsum, saxpy, matrixpartialsum, intproduct, intproduct2
+   public :: intsum, real4sum, real8sum, vectorsum, matrixsum, saxpy, matrixtimesvector
+   public :: averagefnc
 
    integer, parameter :: dp = real64
 
 contains
 
-   subroutine vectorproduct(n, a, b, res)
-      !! Element-wise product of two vectors (explicit shape)
-      integer, intent(in) :: n
-      real(dp), intent(in) :: a(n), b(n)
-      real(dp), intent(out) :: res(n)
-      res = a*b
+   subroutine intsum(a, b, res)
+      !! Sum of two integers
+      integer, intent(in) :: a, b
+      integer, intent(out) :: res
+      res = a + b
+   end subroutine
+
+   subroutine real4sum(a, b, res)
+      !! Sum of two reals
+      real(real32), intent(in) :: a, b
+      real(real32), intent(out) :: res
+      res = a + b
+   end subroutine
+
+   subroutine real8sum(a, b, res)
+      !! Sum of two reals(8)
+      real(real64), intent(in) :: a, b
+      real(real64), intent(out) :: res
+      res = a + b
    end subroutine
 
    subroutine vectorsum(n, a, b, res)
@@ -23,6 +37,22 @@ contains
       real(real32), intent(in) :: a(n), b(n)
       real(real32), intent(out) :: res(n)
       res = a + b
+   end subroutine
+
+   subroutine matrixsum(n, m, a, b, res)
+      !! Element-wise sum of two matrices (explicit shape)
+      integer, intent(in) :: n, m
+      real(dp), intent(in) :: a(n,m), b(n,m)
+      real(dp), intent(out) :: res(n,m)
+      res = a + b
+   end subroutine
+
+   subroutine matrixtimesvector(n, m, a, b, res)
+      !! Product of matrix*vector (explicit shape)
+      integer, intent(in) :: n, m
+      real(real64), intent(in) :: a(n,m), b(m)
+      real(real64), intent(out) :: res(n)
+      res = matmul(a,b)
    end subroutine
 
    subroutine saxpy(n, a, x, y)
@@ -34,33 +64,18 @@ contains
       y = a*x + y
    end subroutine
 
-   subroutine matrixpartialsum(n, m, a, res)
-      !! Partial sum of matrix elements (explicit shape)
-      integer, intent(in) :: n, m
-      real(real64), intent(in) :: a(n, m)
-      real(real64), intent(out) :: res(n, m)
-      integer :: i, j
-      ! Dumb algorithm
-      do j=1,m
-         do i=1,n
-            res(i,j) = sum(a(1:i,1:j))
-         end do
-      end do
+   subroutine averagefnc(fnc, a, b, res)
+      !! Average of function with explicit interface
+      interface 
+         function fnc(x)
+            import dp
+            real(dp), intent(in) :: x
+            real(dp) :: fnc
+         end function
+      end interface
+      real(dp), intent(in) :: a, b
+      real(dp), intent(out) :: res
+      res = (fnc(a) + fnc(b))/2
    end subroutine
-
-   integer function intproduct(a, b) result(res)
-      !! Product of two integers
-      !! This will not work as as expected
-      integer, intent(in) :: a, b
-      res = a*b
-   end function
-
-   function intproduct2(a, b)
-      !! Product of two integers
-      !! This will not work as as expected
-      integer, intent(in) :: a, b
-      integer :: intproduct2
-      intproduct2 = a*b
-   end function
 
 end module mathtools
